0b14515ea7e46859f72ae9d502a4ef26124c3715
Dawid Zbinski <dawid@zbinski.dev>
2026-03-15 11:57:07 +0100

    fix: improve detail panel time range display and test coverage
    Use hyphen instead of en-dash for time range separator, fix padRight
    width to 11 to match actual display width, and guard against zero-value
    Start by falling back to duration-only display. Update tests to assert
    hyphenated ranges, add NotEmpty guards and time range checks to
    SelectedMarker test, add ZeroStart test, and fix TotalMinutes
    computation in helper.

--- internal/cli/report_table_view.go
+++ internal/cli/report_table_view.go
@@ -47,17 +47,21 @@ func (m reportModel) renderDetailPanel() string {
 			marker = "> "
 		}
 
-		fromStr := ce.Start.Format("15:04")
-		toStr := ce.Start.Add(time.Duration(ce.Minutes) * time.Minute).Format("15:04")
-		timeRange := fmt.Sprintf("%s–%s", fromStr, toStr)
-
 		durStr := entry.FormatMinutes(ce.Minutes)
 		msg := ce.Message
 		if msg == "" {
 			msg = "(no message)"
 		}
 
-		line := fmt.Sprintf("%s%s  %s  %s", marker, padRight(timeRange, 13), padRight(durStr, 7), msg)
+		var line string
+		if ce.Start.IsZero() {
+			line = fmt.Sprintf("%s%s  %s", marker, padRight(durStr, 7), msg)
+		} else {
+			fromStr := ce.Start.Format("15:04")
+			toStr := ce.Start.Add(time.Duration(ce.Minutes) * time.Minute).Format("15:04")
+			timeRange := fmt.Sprintf("%s-%s", fromStr, toStr)
+			line = fmt.Sprintf("%s%s  %s  %s", marker, padRight(timeRange, 11), padRight(durStr, 7), msg)
+		}
 		if i == m.selectedEntryIdx {
 			b.WriteString(headerStyle.Render(line))
 		} else {
